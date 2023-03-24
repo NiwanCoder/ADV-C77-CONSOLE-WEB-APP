106470b7bf6cc4110c811b02ab45820b5895fdce
Niwan Raj Bhati <86115960+NiwanCoder@users.noreply.github.com>
2023-03-24 19:14:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My First Console Project");
+var first_name="Niwan";
+var last_name="Raj Bhati";
+console.log(first_name.concat(last_name));
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(add);
+var subtract = a - b;
+console.log(subtract);
+var multiply = a * b;
+console.log(multiply);
+var divide = a / b;
+console.log(divide);
+console.log
